4226afaa7ef4dcd8c3c50e490efdf5771fb0e381
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-09-30 05:33:18 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+  "Hi, my name is Sophie."
+    self.end_with?(".")
   end
 
   def question?
-
+    "Happy Halloween!"
+self.end_with?("?")
   end
 
   def exclamation?
-
+    "Hi, my name is Sophie"
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    complex_string = "one. two. three. four.".split(".").count
+  end
   end
 end
